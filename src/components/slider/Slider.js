e8e86e8e8d10e1d07cbcd0329596382a4fea61a3
chloro30 <chloro30@gmail.com>
2022-01-24 19:22:28 +0900

    .

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,39 +1,58 @@
 import React, {useState} from 'react';
 import './css/Slider.css';
-import BtnSlider from './BtnSlider';
 import dataSlider from './dataSlider';
 
 function Slider() {
 
     const [slideIndex, setSlideIndex] = useState(1);
 
-    const nextSlide = () => {
-        if(slideIndex !== dataSlider.length){
-            setSlideIndex(slideIndex + 1);
-        }else if(slideIndex === dataSlider.length){
+    // const nextSlide = () => {
+    //     if(slideIndex !== dataSlider.length){
+    //         setSlideIndex(slideIndex + 1);
+
+    //     }else if(slideIndex === dataSlider.length){
+    //         setSlideIndex(1);
+    //     }
+    // }
+
+    // const prevSlide = () => {
+    //     if(slideIndex !== 1){
+    //         setSlideIndex(slideIndex - 1);
+
+    //     }else if(slideIndex === 1){
+    //         setSlideIndex(dataSlider.length);
+    //     }
+    // }
+
+    // const clickDot = (index) => {
+    //     setSlideIndex(index);
+    //     console.log(slideIndex, index);
+    // }
+
+
+
+
+// autoPlay 추가
+    setTimeout(() => {
+        if(slideIndex===6){
             setSlideIndex(1);
+            
+        }else{
+            setSlideIndex(slideIndex+1);
         }
+    }, 3000);
 
-    }
 
-    const prevSlide = () => {
-        if(slideIndex !== 1){
-            setSlideIndex(slideIndex - 1);
-        }else if(slideIndex === 1){
-            setSlideIndex(dataSlider.length);
-        }
-    }
 
-    const moveDot = index => {
-        setSlideIndex(index);
-    }
+
 
     return (
         <div className='container-slider'>
+            {/* <div>slideIndex: {slideIndex}</div> */}
             {
                 dataSlider.map((obj, index) => {
                     return (
-                        <div key={obj.id} className={ (slideIndex === index + 1) ? 'slide  active-anim' : 'slide' }>
+                        <div key={obj.id} className={ (slideIndex === index + 1) ? 'slide  active' : 'slide' }>
                             <img src={`img/banner0${index + 1}.png`} alt={`배너_이미지${index + 1}`} />
                             {/* obj.title 사용할 시 css로 위치를 position: absolute로 잡아줘야 할 듯(?) */}
                             {/* <p>{obj.title}</p> */}
@@ -41,12 +60,12 @@ function Slider() {
                     )
                 })
             }
-        <BtnSlider moveSlide={nextSlide} direction={"next"} />
-        <BtnSlider moveSlide={prevSlide} direction={"prev"} />
+        {/* <BtnSlider moveSlide={nextSlide} direction={"next"} />
+        <BtnSlider moveSlide={prevSlide} direction={"prev"} /> */}
         <div className='container-dots'>
-            {Array.from({length: 5}).map((item, index) => (
+            {Array.from({length: 6}).map((item, index) => (
                 <div key={index} 
-                     onClick={ () => moveDot(index + 1) }
+                    //  onClick={ () => clickDot(index + 1) }
                      className={(slideIndex === index + 1) ? 'dot active' : 'dot' }
                 ></div>
             ))}
