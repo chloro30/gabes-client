893db15492a4c90f45213b00327ad9af97f36641
chloro30 <chloro30@gmail.com>
2022-02-21 15:02:22 +0900

    section2에 parallax 추가

--- src/components/main/Section2.js
+++ src/components/main/Section2.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 function Section2() {
+
+    //parallax 적용할 엘리먼트
+    const parallaxImg = useRef();
+    // console.log(parasDiv.current.offsetTop); // null 에러
+
+    const [scrollValue, setScrollValue] = useState(0);
+
+    const [transformValue, setTransformValue] = useState(-130);
+    
+    const onScroll = (e) => {
+
+        // console.log(e.target.scrollingElement.scrollTop);  //리액트에서 scroll 위치 값 얻기
+
+        setScrollValue(e.target.scrollingElement.scrollTop);
+        console.log(`scrollValue: ${scrollValue}`);
+
+    }
+    
+    useEffect(()=>{
+        window.addEventListener('scroll',onScroll);
+
+        if(scrollValue >= 850){
+            setTransformValue(0);
+            console.log(parallaxImg.current.style);
+        }
+
+
+        
+    }, [scrollValue]);
     return (
         <div className='section2'>
             <div className='inner-con'>
                 <div>
-                    <img src='img/main/section2/2022_hawaii_title.png' alt='하와이 텍스트' />
+                    <img ref={parallaxImg}
+                        src='img/main/section2/2022_hawaii_title.png' 
+                        alt='하와이 텍스트'
+                        style={{
+                            transform: `translateX(${transformValue}%)`
+                        }}
+                     />
                     <button>자세히 보기</button>
                 </div>    
             </div>
--- src/components/main/SectionPara.js
+++ src/components/main/SectionPara.js
@@ -13,7 +13,7 @@ function SectionPara() {
         // console.log(e.target.scrollingElement.scrollTop);  //리액트에서 scroll 위치 값 얻기
 
         setScrollValue(e.target.scrollingElement.scrollTop);
-        console.log(`scrollValue: ${scrollValue}`);
+        // console.log(`scrollValue: ${scrollValue}`);
 
     }
     
@@ -31,23 +31,23 @@ function SectionPara() {
             <div className='inner-con'>
                 <div ref={parasDiv} className='paras'>
                     <p style={{
-                        opacity: (scrollValue - 300) / 200}
+                        opacity: (scrollValue - 300) / 300}
                     }>
                         에이리스트
                     </p>
                     <p style={{
-                        opacity: (scrollValue - 400) / 200}
+                        opacity: (scrollValue - 400) / 300}
                     }>
                         TWOSOME<br/>
                         IN YOUR MUG, A LIST
                         </p>
                     <p style={{
-                        opacity: (scrollValue - 500) / 200}
+                        opacity: (scrollValue - 500) / 300}
                     }>
                         언제 어디서나 여유롭게 즐기는 에이리스트
                     </p>
                     <p style={{
-                        opacity: (scrollValue - 600) / 200}
+                        opacity: (scrollValue - 600) / 300}
                     }>
                         좋은 커피를 마셔야 비로소 하루가 시작되는 느낌이라면 하루에도 몇 번씩 그윽한 커피 향이 생각난다면 마음속 우선순위에 언제나 커피가 있는 당신이라면 잊지 말고 챙겨야 할 단 하나의 리스트, 투썸플레이스 에이리스트.
                     </p>
